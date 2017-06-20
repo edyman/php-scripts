031d6a9bf0f8c300c7a161172c6430aff229f993
Edison <edison@cladian.com>
2017-06-19 20:58:58 -0500

    Init

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # php-scripts
 Some integrations and examples php
+
+##Vagrant first configuration##
+$ cd ~/.homestead/ 
+$ vi Homestead.yaml
+
+// Add IP and domain in /etc/hosts
+
+$vagrant up
+$vagrant provision  // execute new changes
---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Sticky Footer Template for Bootstrap</title>
+
+   	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    
+  </head>
+
+  <body>
+
+    <!-- Begin page content -->
+    <div class="container">
+      <div class="page-header">
+        <h1>Php implementations</h1>
+      </div>
+      <article>
+		<h1>Random numbert verify is divided</h1>
+		<p>Implement a random 100 numbers function, verify if the number is divided by 3 and print "Fizz", if number is divided by 5 print "Buzz", if the number is both divided 3 and 5 print "FizzBuzz", in other cases just print the number.
+			
+		</p>
+	</article>
+    </div>
+
+    <footer class="footer">
+      <div class="container">
+        <p class="text-muted">Place sticky footer content here.</p>
+      </div>
+    </footer>
+
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
